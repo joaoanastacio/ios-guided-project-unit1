3aa068d8b5196107b0959108399b0f5dfd8f0985
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-04-18 19:37:36 -0700

    Add final version of project

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lightOn: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUIColor()
     }
 
-
+    fileprivate func updateUIColor() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUIColor()
+    }
 }
 
